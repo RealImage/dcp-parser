3a4ad1c703e0f3ef4d65a4e40c9a663d9451583d
Naveen Kumar M <mnaveen05.04.1999@gmail.com>
2023-03-12 19:46:15 +0530

    Exported commonjs vesion

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ export default {
             format:"esm"
         },
         {
-            file:"dist/index.js",
+            file:"dist/index.cjs",
             format:"cjs"
         }
     ],
